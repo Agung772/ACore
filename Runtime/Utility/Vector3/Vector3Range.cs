2a044c5dc6e0d727c65f1860a1098ef519fa4e2e
Agung Gunawan <agg.nst772@gmail.com>
2026-03-28 15:26:50 +0700

    VerticalGroup

--- Runtime/Utility/Vector3/Vector3Range.cs
+++ Runtime/Utility/Vector3/Vector3Range.cs
@@ -7,10 +7,7 @@ namespace ACore
     [Serializable, InlineProperty]
     public struct Vector3Range
     {
-        [VerticalGroup("Range")]
         public Vector3 min;
-
-        [VerticalGroup("Range")]
         public Vector3 max;
 
         public void Set(Vector3 minValue, Vector3 maxValue)
