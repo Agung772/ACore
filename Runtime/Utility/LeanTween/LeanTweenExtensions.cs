ee0d1b5230348ef824a869ad4dcd320a4cbb751a
Agung Gunawan <agg.nst772@gmail.com>
2026-03-30 10:15:17 +0700

    AnimatedNumber v2

--- Runtime/Utility/LeanTween/LeanTweenExtensions.cs
+++ Runtime/Utility/LeanTween/LeanTweenExtensions.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 namespace ACore
 {
@@ -9,6 +10,15 @@ namespace ACore
         {
             return LeanTween.value(image.gameObject, image.color, to, time)
                 .setOnUpdate((Color c) => { image.color = c; });
+        }
+        
+        public static LTDescr LeanNumber(this GameObject gameObject, int from, int to, 
+            Action<int> onValueChanged, float duration = 0.5f)
+        {
+            return LeanTween.value(gameObject, from, to, duration).setOnUpdate(val => 
+            {
+                onValueChanged?.Invoke(Mathf.RoundToInt(val));
+            });
         }
     }
 }
