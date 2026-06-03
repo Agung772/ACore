a0283acd58ea5a2af7af84405414d53151ff2116
Agung Gunawan <agg.nst772@gmail.com>
2026-06-03 14:00:04 +0700

    Update ScriptableObjectUtility.cs

--- Runtime/Utility/ScriptableObject/ScriptableObjectUtility.cs
+++ Runtime/Utility/ScriptableObject/ScriptableObjectUtility.cs
@@ -1,32 +1,55 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace ACore
 {
     public static class ScriptableObjectUtility
     {
-        private static Dictionary<Type, ScriptableObjectAuto> so = new();
-        
+        private static readonly Dictionary<Type, List<ScriptableObjectAuto>> Cache = new();
+
         public static T GetSO<T>() where T : ScriptableObjectAuto
+        {
+            var _list = GetAllSO<T>();
+            return _list.Count > 0 ? _list[0] : null;
+        }
+
+        public static List<T> GetAllSO<T>() where T : ScriptableObjectAuto
         {
             var _type = typeof(T);
 
-            if (so.TryGetValue(_type, out var _so))
+            if (!Cache.TryGetValue(_type, out var _list))
             {
-                return _so as T;
+                CacheAllSO();
+
+                if (!Cache.TryGetValue(_type, out _list))
+                {
+                    return new List<T>();
+                }
             }
-            
+
+            return _list.Cast<T>().ToList();
+        }
+
+        private static void CacheAllSO()
+        {
+            if (Cache.Count > 0) return;
+
             var _allSO = Resources.LoadAll<ScriptableObjectAuto>("");
+
             foreach (var _item in _allSO)
             {
-                var _itemType = _item.GetType();
-                so.TryAdd(_itemType, _item);
-            }
-                
-            so.TryGetValue(_type, out _so);
+                var _type = _item.GetType();
 
-            return _so as T;
+                if (!Cache.TryGetValue(_type, out var _list))
+                {
+                    _list = new List<ScriptableObjectAuto>();
+                    Cache.Add(_type, _list);
+                }
+
+                _list.Add(_item);
+            }
         }
     }
 }
