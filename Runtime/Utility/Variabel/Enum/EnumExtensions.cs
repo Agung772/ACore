e6491e957c56a6d66922570fe5619ab7d4e68e8d
Agung Gunawan <agg.nst772@gmail.com>
2026-09-12 13:34:11 +0700

    Update EnumExtensions.cs

--- Runtime/Utility/Variabel/Enum/EnumExtensions.cs
+++ Runtime/Utility/Variabel/Enum/EnumExtensions.cs
@@ -28,6 +28,18 @@ namespace ACore
         {
             return enumValue.ToString().ToSpace();
         }
+        
+        public static bool SameName(this Enum enum1, Enum enum2)
+        {
+            if (enum1 == null || enum2 == null) return false;
+            return string.Equals(enum1.ToString(), enum2.ToString(), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool SameValue(this Enum enum1, Enum enum2)
+        {
+            if (enum1 == null || enum2 == null) return false;
+            return enum1.ToInt() == enum2.ToInt();
+        }
     }
 }
 
